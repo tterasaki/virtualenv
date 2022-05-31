3c2b8f3f84d1d5d44d5dc0fa886f7280a3a037d6
tterasaki <tomoki.terasaki@phs.s.u-tokyo.ac.jp>
2022-05-31 09:11:11 +0000

    first commit for my_soenv

--- gen_soenv.sh
+++ gen_soenv.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 py=python3.8
-envname=soenv_$(date +"%Y%m%d")
+envname=tomoki_soenv_$(date +"%Y%m%d")
 echo $py, $envname
 
-sudo virtualenv -p $py /software/$envname
-source /software/$envname/bin/activate
+virtualenv -p $py /homes/tterasaki/my_soenv/software/$envname
+source /homes/tterasaki/my_soenv/software/$envname/bin/
 
-sudo /software/$envname/bin/pip3 install -r requirements.txt
-sudo /software/$envname/bin/$py -m ipykernel install --prefix=/software/$envname/ --name=$envname --display-name=$envname
-sudo /software/$envname/bin/jupyter nbextension enable --py widgetsnbextension --sys-prefix
+/homes/tterasaki/my_soenv/software/$envname/bin/pip3 install -r requirements.txt
+/homes/tterasaki/my_soenv/software/$envname/bin/$py -m ipykernel install --prefix=/homes/tterasaki/my_soenv/software/$envname/ --name=$envname --display-name=$envname
+/homes/tterasaki/my_soenv/software/$envname/bin/jupyter nbextension enable --py widgetsnbextension --sys-prefix
 
 mkdir $envname"_sotodlib"
 cd $envname"_sotodlib"
 git clone git@github.com:simonsobs/sotodlib.git
 cd sotodlib
-sudo /software/$envname/bin/pip3 install .
-/software/$envname/bin/$py setup.py test
+/homes/tterasaki/my_soenv/software/$envname/bin/pip3 install .
+/homes/tterasaki/my_soenv/software/$envname/bin/$py setup.py test
 
-sudo unlink /software/soenv_latest
-sudo ln -s /software/$envname /software/soenv_latest
+#unlink /homes/tterasaki/my_soenv/software/soenv_latest
+#ln -s /homes/tterasaki/my_soenv/software/$envname /homes/tterasaki/my_soenv/software/soenv_latest
