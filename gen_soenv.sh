c22627825688791afbcff504f73a7939bf459b3b
tterasaki <tomoki.terasaki@phs.s.u-tokyo.ac.jp>
2022-05-31 09:16:48 +0000

    added qpoint installation

--- gen_soenv.sh
+++ gen_soenv.sh
@@ -18,5 +18,12 @@ cd sotodlib
 /homes/tterasaki/my_soenv/software/$envname/bin/pip3 install .
 /homes/tterasaki/my_soenv/software/$envname/bin/$py setup.py test
 
-#unlink /homes/tterasaki/my_soenv/software/soenv_latest
-#ln -s /homes/tterasaki/my_soenv/software/$envname /homes/tterasaki/my_soenv/software/soenv_latest
+#2022-05-31 install qpoint (not tested actually)
+mkdir $envname"_qpoint"
+cd $envname"_qpoint"
+git clone git@github.com:arahlin/qpoint.git
+cd qpoint
+/homes/tterasaki/my_soenv/software/$envname/bin/$py setup.py
+
+unlink /homes/tterasaki/my_soenv/software/soenv_latest
+ln -s /homes/tterasaki/my_soenv/software/$envname /homes/tterasaki/my_soenv/software/soenv_latest
